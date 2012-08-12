e287931596d62a9692e660c9c5a6eae0100db626
floyd <floyd@euler.(none)>
2012-08-12 20:16:30 +0100

    Echo server implementation.

---
+++ network/echoserver.hs
@@ -0,0 +1,24 @@
+import Control.Monad
+import Network
+import System.Environment
+import System.IO
+
+main :: IO ()
+main = echoServer `catch` exHandler
+
+echoServer :: IO ()
+echoServer = withSocketsDo $ do
+    args <- getArgs
+    let listenPort = fromIntegral $ (read (head args) :: Int)
+    listenSock <- listenOn $ PortNumber listenPort
+    (handle, host, acceptPort) <- accept listenSock
+    putStrLn $ "accepted connection from " ++ host ++ ":" ++ (show acceptPort)
+    hSetBuffering handle NoBuffering
+    forever $ do
+        input <- hGetLine handle
+        putStrLn $ "received data: " ++ input
+        hPutStrLn handle input
+        putStrLn $ "rending data: " ++ input
+
+exHandler :: IOError -> IO ()
+exHandler e = putStrLn "Houston, we have a problem."
